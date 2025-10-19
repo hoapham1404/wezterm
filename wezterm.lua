3d284b8a0d16448b8628103601d783c15dbfe274
hoapham1404 <phamquanghoa1404@gmail.com>
2025-10-19 11:04:53 +0700

    feat: add little config

--- wezterm.lua
+++ wezterm.lua
@@ -1,39 +1,39 @@
--- Pull in the wezterm API
 local wezterm = require 'wezterm'
-
--- This table will hold the configuration.
 local config = {}
 
--- In newer versions of wezterm, use the config_builder which will
--- help provide clearer error messages
 if wezterm.config_builder then
-  config = wezterm.config_builder()
+	config = wezterm.config_builder()
 end
 
--- This is where you actually apply your config choices
-
 -- Color scheme
-config.color_scheme = 'Dracula'
+config.color_scheme = 'Rosé Pine Dawn (base16)'
 
--- Font configuration
-config.font = wezterm.font('JetBrains Mono', { weight = 'Regular' })
-config.font_size = 12.0
+
+-- Font
+config.font = wezterm.font_with_fallback({
+	{ family = "JetBrains Mono", weight = "Bold", italic = true },
+})
+config.font_size = 16.0
+config.line_height = 1.1
+config.cell_width = 1.0
+config.harfbuzz_features = { "calt=0", "liga=0" }
 
 -- Window configuration
 config.window_background_opacity = 0.95
 config.window_decorations = "RESIZE"
+
 config.window_padding = {
-  left = 2,
-  right = 2,
-  top = 0,
-  bottom = 0,
+	left = 14.0,
+	right = 0,
+	top = 5,
+	bottom = 0,
 }
 
--- Tab bar
+-- Disable tab bar and set initial window size for maximized window
 config.enable_tab_bar = true
-config.hide_tab_bar_if_only_one_tab = false
-config.use_fancy_tab_bar = false
-config.tab_bar_at_bottom = false
+config.initial_rows = 100
+config.initial_cols = 205
+config.window_decorations = "NONE"
 
 -- Cursor
 config.default_cursor_style = 'SteadyBlock'
@@ -41,18 +41,18 @@ config.cursor_blink_rate = 800
 
 -- Keys
 config.keys = {
-  -- Split pane keybindings using the same key with different modifiers
-  -- This is a common pattern in terminal multiplexers
-  {
-    key = 'd',
-    mods = 'CTRL|SHIFT',
-    action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
-  },
-  {
-    key = 'd',
-    mods = 'CTRL|ALT',
-    action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
-  },
+	-- Split pane keybindings using the same key with different modifiers
+	-- This is a common pattern in terminal multiplexers
+	{
+		key = 'd',
+		mods = 'CTRL|SHIFT',
+		action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
+	},
+	{
+		key = 'd',
+		mods = 'CTRL|ALT',
+		action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
+	},
 }
 
 -- and finally, return the configuration to wezterm
