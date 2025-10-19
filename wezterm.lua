cb5b10aa969334d17486cd457a8a3c3584dcb47e
hoapham1404 <phamquanghoa1404@gmail.com>
2025-10-19 11:23:49 +0700

    feat: add config

--- wezterm.lua
+++ wezterm.lua
@@ -1,25 +1,30 @@
 local wezterm = require 'wezterm'
+
+wezterm.on('gui-startup', function(cmd)
+	local _, _, window = wezterm.mux.spawn_window(cmd or {})
+	window:gui_window():maximize()
+end)
 local config = {}
 
 if wezterm.config_builder then
 	config = wezterm.config_builder()
 end
 
+
 -- Color scheme
 config.color_scheme = 'Rosé Pine Dawn (base16)'
-
-
+--
 -- Font
 config.font = wezterm.font_with_fallback({
 	{ family = "JetBrains Mono", weight = "Bold", italic = true },
 })
-config.font_size = 16.0
+config.font_size = 14.0
 config.line_height = 1.1
 config.cell_width = 1.0
 config.harfbuzz_features = { "calt=0", "liga=0" }
 
 -- Window configuration
-config.window_background_opacity = 0.95
+config.window_background_opacity = 0.85
 config.window_decorations = "RESIZE"
 
 config.window_padding = {
@@ -29,11 +34,11 @@ config.window_padding = {
 	bottom = 0,
 }
 
--- Disable tab bar and set initial window size for maximized window
+-- Tab bar
 config.enable_tab_bar = true
-config.initial_rows = 100
-config.initial_cols = 205
-config.window_decorations = "NONE"
+config.hide_tab_bar_if_only_one_tab = false
+config.use_fancy_tab_bar = false
+config.tab_bar_at_bottom = false
 
 -- Cursor
 config.default_cursor_style = 'SteadyBlock'
